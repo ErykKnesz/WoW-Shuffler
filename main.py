62a1999c757970d80a06f2187f89e40469a27292
Eryk Knesz <eryk.knesz@gmail.com>
2022-06-21 20:18:08 +0200

    add next player label and method, add rel paths to empires photos

--- main.py
+++ main.py
@@ -10,17 +10,27 @@ from kivy.properties import (
     StringProperty, ListProperty, BoundedNumericProperty)
 
 
+HORSE_PHOTO = "photos/20220529_200810.png"
+ELEPHANT_PHOTO = "photos/20220529_200833.jpg"
+LION_PHOTO = "photos/20220529_200846.jpg"
+BEAR_PHOTO = "photos/20220529_200856.jpg"
+EAGLE_PHOTO = "photos/20220529_200910.jpg"
+
+
 class MenuScreen(Screen):
     heading = Label(text="Choose shuffling constraints (a or b)")
 
 
 class ShufflingScreen(Screen):
     player_label = Label(text="Current player: Raven")
+    next_player_label = Label(text="Next player: Spider")
     order_label = Label(text="Shuffle the loyalty tokens!")
     next_player_btn = Button(text='Next player', disabled=True)
 
-    def update(self, player_txt, order_txt):
-        self.player_label.text = player_txt
+    def update(self, player_txt, next_player_txt,
+                      order_txt=order_label.text):
+        self.player_label.text = f"Current player: {player_txt}"
+        self.next_player_label.text = f"Next player: {next_player_txt}"
         self.order_label.text = order_txt
 
     def disable_btn(self, btn):
@@ -45,7 +55,8 @@ class WoWShuffler(Widget):
     mode = StringProperty()
     empires = ListProperty(('horse', 'bear', 'lion', 'elephant', 'eagle'))
     players = ListProperty(('Raven', 'Spider', 'Rat', 'Snake'))
-    current_player_index = BoundedNumericProperty(0, min=0, max=3, errorvalue=3)
+    current_player_index = BoundedNumericProperty(0, min=0, max=3, 
+                                                  errorvalue=3)
     current_shuffle = ListProperty([])
     devouts = ListProperty()
     opposed = ListProperty()
@@ -57,7 +68,8 @@ class WoWShuffler(Widget):
     def _shuffled_correctly(self, shuffled):
         if self.mode == 'a':
             return shuffled[0] not in self.devouts
-        return shuffled[0] not in self.devouts and shuffled[-1] not in self.opposed
+        return (shuffled[0] not in self.devouts and shuffled[-1]
+                not in self.opposed)
 
     def get_empires_order(self):
         not_shuffled = True
@@ -77,6 +89,13 @@ class WoWShuffler(Widget):
      
     def get_current_player(self):
         return self.players[self.current_player_index]
+
+    def get_next_player(self):
+        if self.current_player_index < len(self.players) - 1:
+            index = self.current_player_index + 1
+            return self.players[index]
+        return ""
+        
 
     def change_player(self):
         if self.current_shuffle:
